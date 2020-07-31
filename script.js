2f4efdb93a6b48047bee14c5f88d754af5db9f62
Roma <romamelnikov1@gmail.com>
2020-07-31 16:54:34 +0300

    commit

--- script.js
+++ script.js
@@ -1,19 +1,58 @@
-let expression = '0';
-let output = document.getElementById('output-window');
+const COLOR_LIGHT = '#d6d6d6';
+const COLOR_DARK = '#2d2d2d';
 
-let update = () => output.value = expression;
-let clearAll = () => {expression = '0'; update()};
-let equal = () => {expression = eval(expression); update()};
+let expression = '0';
+let darktheme = false;
+let output = document.getElementById('output-window');
+let themeButton = document.getElementById('theme_button');
+let buttons = document.getElementsByClassName('button');
+
+const update = () => output.value = expression;
+const clearAll = () => {
+  expression = '0';
+  update();
+};
+const equal = () => {
+  expression = eval(expression);
+  update();
+};
 
 function clearLast() {
-    if (expression == '0') return true;
-    if (expression.length == 1) expression = '0';
-    else expression = expression.slice(0, -1);
-    update();
+  if (expression == '0') return true;
+  if (expression.length == 1) expression = '0';
+  else expression = expression.slice(0, -1);
+  update();
 }
 
 function input(n) {
-    if (expression == '0' && n != '.') expression = '';
-    expression += n;
-    update();
+  if (expression.length >= 20) return alert("Достигнуто ограничение по количеству символов (20)");
+  if (expression == '0' && n != '.') expression = '';
+  expression += n;
+  update();
+}
+
+function darkEnable() {
+  if (darktheme == false) {
+    document.body.style.backgroundColor = COLOR_DARK;
+    output.style.color = COLOR_LIGHT;
+    output.style.backgroundColor = COLOR_DARK;
+    themeButton.style.color = COLOR_LIGHT;
+    themeButton.style.backgroundColor = COLOR_DARK;
+    themeButton.style.borderColor = COLOR_LIGHT;
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].style.color = COLOR_LIGHT;
+    }
+    darktheme = true;
+  } else {
+    document.body.style.backgroundColor = COLOR_LIGHT;
+    output.style.color = COLOR_DARK;
+    output.style.backgroundColor = COLOR_LIGHT;
+    themeButton.style.color = COLOR_DARK;
+    themeButton.style.backgroundColor = COLOR_LIGHT;
+    themeButton.style.borderColor = COLOR_DARK;
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].style.color = COLOR_DARK;
+    }
+    darktheme = false;
+  }
 }
